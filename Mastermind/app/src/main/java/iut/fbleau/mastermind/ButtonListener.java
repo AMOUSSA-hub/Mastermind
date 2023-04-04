d555bf9c34fc12595ede4182c1f9cb49d049f279
descampsv <victordescamps91@gmail.com>
2023-04-04 23:03:14 +0200

    transition menu choix jeu

--- Mastermind/app/src/main/java/iut/fbleau/mastermind/ButtonListener.java
+++ Mastermind/app/src/main/java/iut/fbleau/mastermind/ButtonListener.java
@@ -1,21 +1,32 @@
 package iut.fbleau.mastermind;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 
 public class ButtonListener implements View.OnClickListener{
 
     Button button;
+    Activity activity;
 
-    public ButtonListener(Button button){
+    public ButtonListener(Button button,Activity activity){
         this.button = button;
+        this.activity = activity;
     }
 
     @Override
     public void onClick(View view) {
-        if(this.button.getId() == R.id.btn_quitter){
+        if(this.button.getId() == R.id.btn_quitter1){
             ((Activity) view.getContext()).finish();
+        }
+        else if(this.button.getId() == R.id.btn_jouer1){
+            Intent intent = new Intent(activity, GameModeActivity.class);
+            activity.startActivity(intent);
+        }
+        else if(this.button.getId() == R.id.btn_retour2){
+            Intent intent = new Intent(activity, MainActivity.class);
+            activity.startActivity(intent);
         }
     }
 
--- Mastermind/app/src/main/java/iut/fbleau/mastermind/MainActivity.java
+++ Mastermind/app/src/main/java/iut/fbleau/mastermind/MainActivity.java
@@ -2,6 +2,7 @@ package iut.fbleau.mastermind;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,8 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnQuitter = (Button) findViewById(R.id.btn_quitter);
-        btnQuitter.setOnClickListener(new ButtonListener(btnQuitter));
+        Button btnQuitter = (Button) findViewById(R.id.btn_quitter1);
+        btnQuitter.setOnClickListener(new ButtonListener(btnQuitter,this));
+        Button btnJouer = (Button) findViewById(R.id.btn_jouer1);
+        btnJouer.setOnClickListener(new ButtonListener(btnJouer,this));
+
     }
 
 
